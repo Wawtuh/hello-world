ef8dc530ad03f8fc556ae4b59d7139768ad23a79
WJS01 <kkkk123won@naver.com>
2020-05-20 11:28:00 +0900

    fin

--- SW-Method-Tools/main.cpp
+++ SW-Method-Tools/main.cpp
@@ -11,8 +11,8 @@ public:
 	void setX(int _x) { x = _x; }
 	void setY(int _y) { y = _y; }
 	void setXY(int _x, int _y) { x = _x; y = _y; }
-	int getX() { return x; }
-	int getY() { return y; }
+	int const getX() { return x; }
+	int const getY() { return y; }
 	Point operator+(const Point& pt2) {
 		Point pt(this->x + pt2.x, this->y + pt2.y);
 		return pt;
@@ -23,11 +23,13 @@ public:
 	}
 };
 
-ostream& operator<<(ostream cout, Point& pt) {
-	cout << "(" << pt.getX() << ", " << ")";
+ostream& operator<<(ostream& cout, Point pt) {
+	cout << "(" << pt.getX() << ", " << pt.getY() << ")";
 	return cout;
 }
 
 int main() {
-
+	Point pt1(1,2), pt2(2,3);
+	cout << pt1 + pt2 << endl;
+	cout << pt1 - pt2 << endl;
 }
